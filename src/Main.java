28b098da2a274854067381aa9e3be52f23f77ccb
abd1 <abd1@gmail.com>
2024-06-19 06:28:39 +0300

    feature-1 commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        int[] arr={1,2,3};
+        printIntigers(arr);
     }
     public static void printIntigers(int[]arr){
         for (int i = 0; i < arr.length; i++) {
